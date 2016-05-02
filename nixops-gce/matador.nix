035bffa09f6ca2a0e2d4f5e75e1e47a7facf0082
Corbin <cds@corbinsimpson.com>
2016-05-02 14:06:25 -0700

    Disable password authentication for SSH.

--- nixops-gce/matador.nix
+++ nixops-gce/matador.nix
@@ -32,10 +32,15 @@ let
   
     # System services.
     services = {
-      # Stop annoying SSH attempts. This is pretty much essential for GCE, as
-      # otherwise your machines will spend lots of precious CPU time and
-      # network traffic responding to SSH attempts from scanners.
+      # Stop annoying SSH attempts, part one. This is pretty much essential
+      # for GCE, as otherwise your machines will spend lots of precious CPU
+      # time and network traffic responding to SSH attempts from scanners.
       fail2ban.enable = true;
+
+      # Stop annoying SSH attempts, part two. The attackers seem to almost
+      # always use password-interactive authentication, but nixops uses keys;
+      # we can cut them off easily.
+      openssh.passwordAuthentication = false;
 
       # Main Tahoe node configuration.
       tahoe.nodes."storage-${nodeName}" = {
